cd0bbefca02a5adbc6b6f7f52eaee816a9ec5aae
Josiane Morissette <jomo.music.123@gmail.com>
2022-05-20 17:37:33 -0400

    💄 Create a collapsible component for the sidebar.

---
+++ src/lib/Collapsible.svelte
@@ -0,0 +1,23 @@
+<!-- Options: collapsible-plus, collapsible-arrow, collapsible-sm-plus collapsible-sm-arrow-->
+<script lang="ts">
+	import '/static/collapsible_styled.css';
+	import '/static/collapsible_unstyled.css';
+	let componentClass = '';
+	export { componentClass as class };
+
+	export let open = false;
+
+	export function handleClick() {
+		open = !open;
+	}
+</script>
+
+<div class="collapsible {componentClass} ">
+	<input type="checkbox" on:click={handleClick} />
+	<div class="collapsible-title">
+		<slot name="title" />
+	</div>
+	<div class="collapsible-content ">
+		<slot name="content" />
+	</div>
+</div>
---
+++ src/lib/sidebar/SidebarCollapsible.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Collapsible from '$lib/Collapsible.svelte';
+
+	export let title = 'title';
+</script>
+
+<Collapsible
+	class="collapsible-arrow text-gray-200 hover:text-primary capitalize w-full text-lg tracking-wide font-normal"
+>
+	<div slot="title">
+		<div class="flex items-center space-x-2  ">
+			<div class="h-6 w-6">
+				<slot name="icon" />
+			</div>
+
+			<div>{title}</div>
+		</div>
+	</div>
+
+	<div slot="content">
+		<slot name="content" />
+	</div>
+</Collapsible>
