5c23eb931ee1d6e92b03a4356ad58cce2d6edebb
Josiane Morissette <jomo.music.123@gmail.com>
2022-05-26 19:24:16 -0400

    💄 Create a button animation.

---
+++ src/lib/actions/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '/static/button_animation.css';
+	export let componentClass = '';
+
+	export let handleClick = () => {};
+</script>
+
+<button
+	on:click={handleClick}
+	class="{componentClass} hover:text-primary button flex items-center rounded-md"
+>
+	<slot>Empty button</slot>
+</button>
---
+++ src/lib/actions/LinkButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/actions/Button.svelte';
+
+	import '/static/button_animation.css';
+	export let componentClass = 'hover:text-red-300 ';
+
+	export let href: string;
+	function handleClick() {
+		goto(href);
+	}
+</script>
+
+<Button {handleClick} {componentClass}><slot /></Button>
