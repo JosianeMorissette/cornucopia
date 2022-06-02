b2c6f64acc9d9f3ea687e03ef86bf4d2c2b74b47
Josiane Morissette <jomo.music.123@gmail.com>
2022-06-01 22:45:39 -0400

    ♻️ Remove DaisyUi from the dependency.

--- src/lib/tags/Button.svelte
+++ src/lib/tags/Button.svelte
@@ -1,7 +1,11 @@
+<script>
+	import Button from '$lib/actions/Button.svelte';
+</script>
+
 <div class="">
-	<button class="btn btn-primary btn-sm w-1/2 normal-case rounded-full ">
-		<div class="h-6 flex items-center ">
-			<slot>empty button</slot>
-		</div></button
+	<Button
+		componentClass="w-1/2 flex justify-center  my-6 max-w-3xl bg-primary text-primary-content rounded-full "
 	>
+		<slot>empty button</slot>
+	</Button>
 </div>
