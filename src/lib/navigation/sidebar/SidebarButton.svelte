3276b3e34c8bcb696a369d20c5d442c0fa56f94a
Josiane Morissette <jomo.music.123@gmail.com>
2022-06-13 14:21:00 -0400

    ♻️ New design for the doc sidebar.

--- src/lib/navigation/sidebar/SidebarButton.svelte
+++ src/lib/navigation/sidebar/SidebarButton.svelte
@@ -1,47 +1,36 @@
 <script lang="ts">
-	import type { AboutLayoutLink } from '$lib/utils/appState';
+	import type { LayoutLink } from '$lib/utils/appState';
 	import { appState } from '$lib/utils/appState';
 	import Button from '$lib/actions/Button.svelte';
 	import { goto } from '$app/navigation';
-
-	export let link: AboutLayoutLink;
-
-	export let collapsible = false;
+	export let link: LayoutLink;
 
 	function handleClick() {
-		$appState.sidebar.index = link.index;
+		console.log($appState);
 		$appState.sidebarOpen = false;
 		goto(link.href);
-		console.log($appState.sidebar.index);
+		console.log($appState);
 	}
 
 	let selected = link.index === $appState.sidebar.index;
 
 	let btnClass =
-		' font-normal text-left text-lg rounded-none border-0 tracking-wider font-normal p-3 justify-start capitalize w-full flex items-center';
+		' font-normal text-left text-lg rounded-none border-0 tracking-wider font-normal py-4 justify-start capitalize w-full flex items-center';
 
-	let componentClass: string;
+	export let componentClass = '';
 
 	$: if (selected) {
 		componentClass = 'text-primary';
 	} else {
-		componentClass = 'text-zinc-200 hover:text-zinc-100 hover:text-white ';
+		componentClass = 'text-zinc-300 hover:text-white hover:text-white ';
 	}
 </script>
 
-{#if collapsible}
-	<Button componentClass=" pl-[3.25rem] {componentClass} {btnClass}" {handleClick}>
-		<div class="flex items-center flex-grow text-left">
-			{link.label}
-		</div>
-	</Button>
-{:else}
-	<Button componentClass=" {btnClass} {componentClass}" {handleClick}>
-		<div class="mr-4 h-6 w-6 ">
-			<slot name="icon" />
-		</div>
-		<div class="pr-8 flex items-center flex-grow ">
-			{link.label}
-		</div>
-	</Button>
-{/if}
+<Button componentClass=" {btnClass} {componentClass}" {handleClick}>
+	<div class="mr-4 h-6 w-6 opacity-80">
+		<slot name="icon" />
+	</div>
+	<div class="pr-8 flex items-center flex-grow ">
+		{link.label}
+	</div>
+</Button>
---
+++ src/lib/navigation/sidebar/SidebarTitle.svelte
@@ -0,0 +1,3 @@
+<h2 class="text-xl font-semibold mb-3 tracking-wide">
+	<slot />
+</h2>
